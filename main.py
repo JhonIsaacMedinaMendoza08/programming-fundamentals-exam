9ae052f222ec1d4f1aec36897c489edcda938069
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-05 12:21:37 -0500

    Solution exercise 2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-volt1 = int(input("enter voltage: "))
-volt2 = int(input("enter voltage: "))
-volt3 = int(input("enter voltage: "))
-volt4 = int(input("enter voltage: "))
-volt5 = int(input("enter voltage: "))
-VoltajeFInal = ((volt1+volt2+volt3+volt4+volt5)/5)
-if VoltajeFInal > 220:
-    print ("HIGH VOLTAGE")
+import math
+print ("Exercise 2 area of equilateral triangles")
+
+lado1 = float(input("enter side A of the equilateral triangle: "))
+
+area = ((1.7320)/4)* math.pow(lado1, 2)
+
+if area < 1000:
+    print (f"The area of the triangle is: {area} cm")
 else:
-    print ("CORRECT VOLTAGE")
+    print ("INVALID DATA")
+    

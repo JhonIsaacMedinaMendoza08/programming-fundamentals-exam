6f5fd979d3abdd3743ee2e052d7c1ea3696e7aa5
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-05 12:22:19 -0500

    Solution exercise 4

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-print ("Exercise 3 Voltages")
-print ("Please, Enter different voltages")
-volt1 = int(input("Enter voltage: "))
-volt2 = int(input("Enter voltage: "))
-volt3 = int(input("Enter voltage: "))
+print ("Solution Exercise 4 Meters - Kilometers")
 
-prom = (volt1+volt2+volt3)/3
+meters = float(input("Enter the distance in meters please: "))
 
-if volt1 != volt2 and volt1 != volt3 and volt2 !=volt3:
-    if prom < 115:
-        print (f"CORRECT VOLTAGE {prom}")
-    elif prom >115 and prom < 220:
-        print (f"CORRECT VOLTAGE {prom}")
-    else:
-        print (f"DANGEROUS {prom}")
-else:
-    print ("Please, enter different voltages")
+kilometers = meters/1000
+
+print (f"{meters} meters are {kilometers} kilometers ")
     
